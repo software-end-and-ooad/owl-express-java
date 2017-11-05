94d20fb97d7c5e0469d9673f11d5edbbee5dc2f5
Jitiwattana <bbos1@windowslive.com>
2017-11-06 02:45:39 +0700

    FIX project cannot open after clone by nbproject

--- src/owlexpress/LoginController.java
+++ src/owlexpress/LoginController.java
@@ -5,12 +5,21 @@
  */
 package owlexpress;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -31,11 +40,22 @@ public class LoginController implements Initializable {
         // TODO
     }   
     
-    public void submitLogin() {
+    public void submitLogin() throws IOException {
          if(username_field.getText().compareTo("root") == 0 && password_field.getText().compareTo("root") == 0)
-            System.out.println("Authentication Passed! :D");
+         {
+             Stage stage;
+             Parent root;
+             stage=(Stage) username_field.getScene().getWindow();
+             //load up OTHER FXML document
+             root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+             //create a new scene with root and set the stage
+             Scene scene = new Scene(root);
+             stage.setScene(scene);
+             stage.show();
+         }    
          else
              System.out.println("Authentication Invalid!");
     }
+    
     
 }
