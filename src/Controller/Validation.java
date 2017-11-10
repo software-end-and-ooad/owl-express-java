d0eb343e20c49f51d990030cb7c9a0339d9a73da
Wisoodthipong Takeow <wttakeow@gmail.com>
2017-11-10 14:26:26 +0700

    redo

--- src/Controller/Validation.java
+++ src/Controller/Validation.java
@@ -17,4 +17,37 @@ public class Validation {
         return true;
     }
     
+    public boolean minLength(String input, int min) {
+        if (input.length() < min) 
+            return false;
+        return true;
+    }
+    
+    
+    public boolean isNumeric(String str)
+    {
+        for (char c : str.toCharArray())
+        {
+        if (!Character.isDigit(c)) return false;
+        }
+        return true;
+    }
+    
+    public boolean isChar(String str)
+    {
+        for (char c : str.toCharArray())
+        {
+        if (Character.isDigit(c)) return false;
+        }
+        return true;
+    }
+    
+    public boolean isValidEmailAddress(String email) {
+           String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+           java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+           java.util.regex.Matcher m = p.matcher(email);
+           return m.matches();
+    }
+    
+    
 }
