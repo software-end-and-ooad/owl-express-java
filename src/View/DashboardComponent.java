4706e2cb34bb000c5005e64dc1eff9b55fc675b6
Jitiwattana <bbos1@windowslive.com>
2017-11-15 22:37:25 +0700

    UPDATE dashboard to be template left menu

--- src/View/DashboardComponent.java
+++ src/View/DashboardComponent.java
@@ -8,11 +8,15 @@ package View;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
@@ -21,6 +25,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
  *
@@ -31,62 +36,22 @@ public class DashboardComponent implements Initializable {
     @FXML
     private ImageView  btn_user, btn_noti, btn_oder, btn_exit, exitlogin_button;
     @FXML
-    private AnchorPane home, user, notification, oder, login, btn_home;
-    @FXML
-    private ListView notiList;
-    @FXML
-    private JFXTextField name_field;
-    @FXML
-    private Text forgotPass;
+    private AnchorPane scene_area;
     @FXML
     private JFXButton order_button, refresh_button, login_button, signup_button;
     
-    public String page;
-    
     @FXML
     private void pageButton(MouseEvent event) {
-        if(event.getTarget() == btn_home){
-            home.setVisible(true);
-            user.setVisible(false);
-            notification.setVisible(false);
-            oder.setVisible(false);
-        }
-        else if(event.getTarget() == btn_user){
-            home.setVisible(false);
-            user.setVisible(true);
-            notification.setVisible(false);
-            oder.setVisible(false);
-        }
-        else if(event.getTarget() == btn_noti){
-            home.setVisible(false);
-            user.setVisible(false);
-            notification.setVisible(true);
-            oder.setVisible(false);
-        }
-        else if(event.getTarget() == btn_oder){
-            home.setVisible(false);
-            user.setVisible(false);
-            notification.setVisible(false);
-            oder.setVisible(true);
-        }
-        else if(event.getTarget() == btn_exit){
+        
+         if(event.getTarget() == btn_exit){
             System.exit(0);
         }
     }
+    
     @FXML
-    private void orderButton(MouseEvent event) {
-        //Store data
-        System.out.print(name_field.getText());
-        name_field.clear();
-    }
-    @FXML
-    private void refresh(MouseEvent event){
-        //Show updated status
-        notiList.getItems().addAll("list");
-    }
-    @FXML
-    private void loginButton(MouseEvent event){
-       
+    private void orderButton(MouseEvent event) throws IOException {
+        AnchorPane screen_page = FXMLLoader.load(this.getClass().getResource("LoginComponent.fxml"));
+        this.scene_area.getChildren().setAll(screen_page);
     }
 
     @Override
